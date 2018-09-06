bb9707f075a90df81ac5bfda9a4959ed4782cf96
grsubin <963subin@gmail.com>
2018-09-06 20:41:51 +0545

    swagger

--- src/main/java/com/texas/CustomerBehaviorSystem/config/SwaggerConfig.java
+++ src/main/java/com/texas/CustomerBehaviorSystem/config/SwaggerConfig.java
@@ -68,12 +68,13 @@ public class SwaggerConfig extends WebMvcConfigurerAdapter {
 
 	
 	@Override
-	public void addResourceHandlers(final ResourceHandlerRegistry registry) {
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
 
+		
 		registry.addResourceHandler("swagger-ui.html").addResourceLocations("classpath:/META-INF/resources/");
 
 		registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
-
+		
 	}
 
 	@Bean	
